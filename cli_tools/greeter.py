fc074c58108df1ca23fb4dbc5a8cc3e7448a6182
Harikrishan Narayanan <harikrishan.narayanan@tryg.no>
2022-03-07 21:04:09 +0000

    Added greeter script

--- cli_tools/greeter.py
+++ cli_tools/greeter.py
@@ -1,4 +1,10 @@
 # greeter.py
 
-def greet():
-    print("Hello")
+import click
+
+@click.command()
+@click.argument('first_name')
+@click.argument('last_name')
+def greet(first_name, last_name):
+    """Displays a greeting to the user"""
+    click.echo(f"Hello, {first_name} {last_name}!")
